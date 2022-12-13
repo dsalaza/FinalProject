f689072a5796a1bcd04c4691a213a173b24c9bf4
Devon <devon.m.salazar@gmail.com>
2022-12-13 02:36:51 -0700

    Create listTeams(). Fetch team list from API and crossreference against input value. If a match is found, team ID is passed into listRoster() as argument.

--- index.js
+++ index.js
@@ -5,3 +5,22 @@ document.querySelector(`form`).addEventListener(`submit`, (e) => {
     e.preventDefault()
     listTeams(e.target.searchBar.value)
 })
+
+//takes input from search bar, checks to see if the input is equal to a team name in the 
+//database, then runs listRoster() using the teamID
+
+function listTeams (team) {
+
+    fetch(`http://lookup-service-prod.mlb.com/json/named.team_all_season.bam?sport_code='mlb'&sort_order=name_asc&season=2022`)
+    .then(resp => resp.json())
+    .then(data => { 
+        
+        let teamArray = data.team_all_season.queryResults.row
+        teamArray.find(element => {
+
+            if (element.name === team) {
+                listRoster(element.team_id)
+            }
+        })
+    })
+}
